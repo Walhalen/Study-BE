1b91e2dcd3551afe3431ec9c80feb150ce297f8b
exit <exit>
2023-08-16 19:26:38 +0300

    First Commit

---
+++ src/main/java/com/diplomaProject/StudyBe/StudyBeApplication.java
@@ -0,0 +1,13 @@
+package com.diplomaProject.StudyBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplomaProject/StudyBe/StudyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplomaProject.StudyBe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
