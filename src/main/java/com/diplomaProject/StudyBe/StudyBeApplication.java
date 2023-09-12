478454cc1fa174855e7b1ce7445a3823fb004c45
exit <exit>
2023-09-13 01:05:34 +0300

    User-repo,service,dto,registration controller

--- src/main/java/com/diplomaProject/StudyBe/StudyBeApplication.java
+++ src/main/java/com/diplomaProject/StudyBe/StudyBeApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {
+//		"com.diplomaProject.StudyBe.User.Controller",
+//
+//
+//})
 public class StudyBeApplication {
 
 	public static void main(String[] args) {
